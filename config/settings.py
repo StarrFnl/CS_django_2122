6661fc3203aec3c6974f2fcc0299819ed973d0c8
ybkim3603 <ybkim3603@gmail.com>
2022-01-23 20:16:49 +0900

    Docs: settings.py에 CommonConfig App 추가

--- config/settings.py
+++ config/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
     'proj.apps.ProjConfig',
     'django.contrib.admin',
     'django.contrib.auth',
